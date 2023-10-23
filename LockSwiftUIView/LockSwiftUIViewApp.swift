df072fafb7bfd5c91a7846077c1bd6d72f1dca44
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-23 17:58:48 +0300

    Initial Commit

---
+++ LockSwiftUIView/LockSwiftUIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockSwiftUIViewApp.swift
+//  LockSwiftUIView
+//
+//  Created by Yunus Emre Taşdemir on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LockSwiftUIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
