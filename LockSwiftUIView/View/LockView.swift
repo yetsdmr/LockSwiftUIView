4ce845607facdccc785683ae876d9d422032bfaf
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-11-14 14:13:28 +0300

    Building NumberPad Lock View Interface.

--- LockSwiftUIView/View/LockView.swift
+++ LockSwiftUIView/View/LockView.swift
@@ -15,6 +15,12 @@ struct LockView<Content: View>: View {
     var isEnable: Bool
     var lockWhenAppGoesBackground: Bool = true
     @ViewBuilder var content: Content
+    var forgotPin: () -> () = { }
+    // View Properties
+    @State private var pin: String = ""
+    @State private var animateField: Bool = false
+    @State private var isUnlocked: Bool = false
+    @State private var noBiometricAccess: Bool = false
     
     var body: some View {
         GeometryReader {
@@ -23,17 +29,145 @@ struct LockView<Content: View>: View {
             content
                 .frame(width: size.width, height: size.height)
             
-            if isEnable {
+            if isEnable && isUnlocked {
                 ZStack {
+                    Rectangle()
+                        .ignoresSafeArea()
+                    
                     if lockType == .both || lockType == .biometric {
-                        Rectangle()
+                        
                     } else {
                         // Custom Number Pad to type View Lock Pin
+                        numberPadPinView()
                     }
                 }
-                .ignoresSafeArea()
+                .transition(.offset(y: size.height + 100))
+                
             }
         }
+    }
+    
+    // Numberpad Pin View
+    @ViewBuilder
+    func numberPadPinView() -> some View {
+        VStack(spacing: 15) {
+            Text("Enter Pin")
+                .font(.title.bold())
+                .frame(maxWidth: .infinity)
+            
+            // Adding Winggling Animation for Wrong Password With Keyframe Animator
+            HStack(spacing: 10) {
+                ForEach(0..<4, id: \.self) { index in
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 50, height: 55)
+                        // Showing Pin at each box with the help of Index
+                        .overlay {
+                            // Safe Check
+                            if pin.count > index {
+                                let index = pin.index(pin.startIndex, offsetBy: index)
+                                let string = String(pin[index])
+                                
+                                Text(string)
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundStyle(.black)
+                            }
+                        }
+                    
+                }
+            }
+            .keyframeAnimator(initialValue: CGFloat.zero, trigger: animateField, content: { content, value in
+                content.offset(x: value)
+            }, keyframes: { _ in
+                KeyframeTrack {
+                    CubicKeyframe(30, duration: 0.07)
+                    CubicKeyframe(-30, duration: 0.07)
+                    CubicKeyframe(20, duration: 0.07)
+                    CubicKeyframe(-20, duration: 0.07)
+                    CubicKeyframe(0, duration: 0.07)
+                }
+            })
+            .padding(.top, 15)
+            .overlay(alignment: .bottomTrailing, content: {
+                Button("Forgot pin?", action: forgotPin)
+                    .foregroundStyle(.white)
+                    .offset(y: 40)
+            })
+            .frame(maxHeight: .infinity)
+            
+            // Custom Number Pad
+            GeometryReader { _ in
+                LazyVGrid(columns: Array(repeating: GridItem(), count: 3), content: {
+                    ForEach(1...9, id: \.self) { number in
+                        Button(action: {
+                            // Adding Number to Pin
+                            // Max Limit - 4
+                            if pin.count < 4 {
+                                pin.append("\(number)")
+                            }
+                        }, label: {
+                            Text("\(number)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 20)
+                                .contentShape(.rect)
+                        })
+                        .tint(.white)
+                    }
+                    
+                    // 0 and Back Button
+                    Button(action: {
+                        if !pin.isEmpty {
+                            pin.removeLast()
+                        }
+                    }, label: {
+                        Image(systemName: "delete.backward")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 20)
+                            .contentShape(.rect)
+                    })
+                    .tint(.white)
+                    
+                    Button(action: {
+                        if pin.count < 4 {
+                            pin.append("0")
+                        }
+                    }, label: {
+                        Text("0")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .padding(.vertical, 20)
+                            .contentShape(.rect)
+                    })
+                    .tint(.white)
+                   
+                    
+                    
+                })
+                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+            .onChange(of: pin) { oldValue, newValue in
+                if newValue.count == 4 {
+                    // Validate Pin
+                    if lockPin == pin {
+                        //print("Unlocked")
+                        withAnimation(.snappy, completionCriteria: .logicallyComplete) {
+                            isUnlocked = true
+                        } completion: {
+                            // Clearing Pin
+                            pin = ""
+                        }
+                    } else {
+                        //print("Wrong Pin")
+                        pin = ""
+                        animateField.toggle()
+                    }
+                }
+            }
+        }
+        .padding()
+        .environment(\.colorScheme, .dark)
     }
     
     // Lock Type
